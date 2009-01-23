8d0764a514f241275a82e4430aac52b7f592b671
Roy Marples <roy@marples.name>
2009-01-23 01:33:38 +0000

    Release 0.1.2

--- config.h
+++ config.h
@@ -28,7 +28,7 @@
 #define CONFIG_H
 
 #define PACKAGE			"dhcpcd-gtk"
-#define VERSION			"0.1.2"
+#define VERSION			"0.1.3"
 
 #define DHCPCD_SERVICE		"name.marples.roy.dhcpcd"
 #define DHCPCD_PATH		"/name/marples/roy/dhcpcd"
