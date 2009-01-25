23a4739824f9f939d33ded050bd1590f9f117d8a
Roy Marples <roy@marples.name>
2009-01-25 13:49:47 +0000

    There is no need to show the license on the help about dialog.

--- menu.c
+++ menu.c
@@ -33,29 +33,6 @@ static const char *authors[] = {
 	"Roy Marples <roy@marples.name>",
 	NULL
 };
-static const char *license[] = {
-	"Licensed under the 2 clause BSD license.\n",
-	"Redistribution and use in source and binary forms, with or without\n"
-	"modification, are permitted provided that the following conditions\n"
-	"are met:\n",
-	"1. Redistributions of source code must retain the above copyright\n"
-	"   notice, this list of conditions and the following disclaimer.\n",
-	"2. Redistributions in binary form must reproduce the above copyright\n"
-	"   notice, this list of conditions and the following disclaimer in the\n"
-	"   documentation and/or other materials provided with the distribution.\n",
-	"THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND\n"
-	"ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n"
-	"IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE\n"
-	"ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE\n"
-	"FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n"
-	"DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS\n"
-	"OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)\n"
-	"HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT\n"
-	"LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY\n"
-	"OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF\n"
-	"SUCH DAMAGE.",
-	NULL
-};
 
 static void
 on_quit(_unused GtkMenuItem *item, _unused gpointer data)
@@ -96,27 +73,17 @@ url_hook(GtkAboutDialog *dialog, const char *url, _unused gpointer p)
 static void
 on_about(_unused GtkMenuItem *item, _unused gpointer data)
 {
-	char *l;
-
-	l = g_strconcat(_(license[0]), "\n",
-			_(license[1]), "\n",
-			_(license[2]), "\n",
-			_(license[3]), "\n",
-			_(license[4]), "\n",
-			NULL);
 	gtk_window_set_default_icon_name(GTK_STOCK_NETWORK);
 	gtk_about_dialog_set_email_hook(email_hook, NULL, NULL);
 	gtk_about_dialog_set_url_hook(url_hook, NULL, NULL);
 	gtk_show_about_dialog(NULL,
 			      "version", VERSION,
 			      "copyright", copyright,
-			      "license", l,
 			      "website-label", "dhcpcd Website",
 			      "website", "http://roy.marples.name/projects/dhcpcd",
 			      "authors", authors,
 			      "logo-icon-name", GTK_STOCK_NETWORK,
 			      NULL);
-	g_free(l);
 }
 
 static void
