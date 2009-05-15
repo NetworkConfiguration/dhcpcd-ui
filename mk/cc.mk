4687e8bec5520b9648413fbc5519c03e547a218c
Roy Marples <roy@marples.name>
2009-05-15 09:09:18 +0000

    Can't easily test for git, so just use the CFLAGS.

--- mk/cc.mk
+++ mk/cc.mk
@@ -3,7 +3,7 @@
 # Setup some good default CFLAGS
 CFLAGS?=	-O2
 
-# Try and use some good cc flags if we're building from svn
+# Try and use some good cc flags
 # We don't use -pedantic as it will warn about our perfectly valid
 # use of %m in our logger.
 _CCFLAGS=	-Wall -Wextra -Wimplicit -Wshadow -Wformat=2 \
@@ -13,7 +13,7 @@ _CCFLAGS=	-Wall -Wextra -Wimplicit -Wshadow -Wformat=2 \
 		-Winline -Wwrite-strings -Wcast-align -Wcast-qual \
 		-Wpointer-arith \
 		-Wdeclaration-after-statement -Wsequence-point
-_CC_FLAGS_SH=	if ! test -d .svn; then echo ""; else for f in ${_CCFLAGS}; do \
+_CC_FLAGS_SH=	for f in ${_CCFLAGS}; do \
 		if echo "int main(void) { return 0;} " | \
 		${CC} $$f -S -xc -o /dev/null - ; \
 		then printf "%s" "$$f "; fi \
